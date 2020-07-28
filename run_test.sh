8a46224ce1de682afca57209794018fe079a73b6
Cole Killian <g.cole.killian@gmail.com>
2020-07-28 02:58:01 +0000

    Modify run_test.sh to apply on a list of models

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,7 @@
-python3 test_on_image.py	--image_path images/content/zurich.jpeg \
-				--checkpoint_model starry_night_10000.pth
+models="blurflower cuphead flower japan spiral starry_night gear jerry mosaic psycwolf spiral the_wave"
+
+for model in $models; do
+
+	python3 test_on_image.py --image_path images/content/zurich.jpeg \
+			 --checkpoint_model checkpoints/${model}_20000.pth
+done
